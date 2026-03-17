cb8786d6d36d1ec96f26e4d37396d6c7ffbb4066
Arthur Berezin <rtpro1@gmail.com>
2026-03-17 19:25:20 +0200

    Update workout icons to body-part silhouette style

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "gymmer-v53";
+const CACHE_NAME = "gymmer-v54";
 const ASSETS = ["index.html", "styles.css", "app.js", "manifest.json", "icon.svg"];
 
 self.addEventListener("install", function (e) {
