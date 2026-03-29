b7633c00af82b815d93cda96feaaa6091ba2409e
Arthur Berezin <rtpro1@gmail.com>
2026-03-29 20:19:38 +0300

    Group phase badge with muscle indicator

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "gymmer-v57";
+const CACHE_NAME = "gymmer-v58";
 const ASSETS = ["index.html", "styles.css", "app.js", "manifest.json", "icon.svg"];
 
 self.addEventListener("install", function (e) {
