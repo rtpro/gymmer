2777da010d7b58d79d03f0cf9ea5051d530929fc
Arthur Berezin <rtpro1@gmail.com>
2026-05-30 17:08:21 +0300

    Enlarge timer status pill for distance readability

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "gymmer-v76";
+const CACHE_NAME = "gymmer-v77";
 const ASSETS = ["index.html", "styles.css", "app.js", "manifest.json", "icon.svg"];
 
 self.addEventListener("install", function (e) {
