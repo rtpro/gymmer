906d6e92e61503bb83db4fa87fe95c0c67ad89dd
Arthur Berezin <rtpro1@gmail.com>
2026-05-31 19:27:16 +0300

    Make set dots larger and timer-inspired

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "gymmer-v79";
+const CACHE_NAME = "gymmer-v80";
 const ASSETS = ["index.html", "styles.css", "app.js", "manifest.json", "icon.svg"];
 
 self.addEventListener("install", function (e) {
