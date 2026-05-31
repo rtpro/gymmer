8242403a155e513f96158fdd33d1b0e09c4df48e
Arthur Berezin <rtpro1@gmail.com>
2026-05-31 19:42:35 +0300

    Make set circles larger and closer to timer design

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "gymmer-v82";
+const CACHE_NAME = "gymmer-v83";
 const ASSETS = ["index.html", "styles.css", "app.js", "manifest.json", "icon.svg"];
 
 self.addEventListener("install", function (e) {
